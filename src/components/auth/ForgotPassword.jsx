585c5cd13800b7948078fddb37a3ab0aa206deb6
Ronal Sánchez <ronal.sanx21@gmail.com>
2025-11-15 08:43:51 -0500

    Cambio iconos

--- src/components/auth/ForgotPassword.jsx
+++ src/components/auth/ForgotPassword.jsx
@@ -1,11 +1,33 @@
+import { FiUser } from "react-icons/fi";
 import Content from "./Content"
+import { Link } from 'react-router-dom'
+import { useState } from "react";
+
+
 
 const ForgotPassword = () => {
+
+
   return (
     <div className="flex">
-      <Content/>
-      <div className="w-[600px] bg-white m-40 md:mt-40 h-fit rounded-lg p-4 shadow-lg">
-        <h1>Forgot Password</h1>
+      <Content />
+      <div className="bg-white m w-[500px] md:mt-40 ml-20 mt-32 md:ml-20 h-fit rounded-lg p-8 shadow-lg">
+        <h1>Recuperar Contraseña</h1>
+        <p className="text-sm text-(--color-text) mb-10">Recupere su contraseña enviando un correo electronico</p>
+
+        <form>
+          <div className="relative">
+
+            <FiUser className='absolute top-2 ml-1 size-6 text-[#2563EB]' />
+            <input className='rounded-lg p-2 border-0 bg-gray-100 pl-8 w-full' type="email" placeholder='email@empresa.com' />
+          </div>
+
+         
+          <button className='bg-(--color-primary) w-full text-white rounded-lg p-2 mt-6 hover:bg-(--color-primary-2) transition-colors cursor-pointer'>Enviar</button>
+
+        </form>
+
+
       </div>
     </div>
 
