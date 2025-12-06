26680f8f47382e4b3d4a899f2db261db09c2f9d0
Ronal Sánchez <ronal.sanx21@gmail.com>
2025-12-06 09:54:27 -0500

    add component Page

---
+++ src/components/Admin/TecnicosDisponibles.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TecnicosDisponibles = () => {
+  return (
+    <div className='bg-white shadow-md w-[35%]'>
+      Tecnicos disponibles
+    </div>
+  )
+}
+
+export default TecnicosDisponibles
--- src/pages/AsignacionTicketPage.jsx
+++ src/pages/AsignacionTicketPage.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import TicketsStats from '../components/Admin/TicketsStats'
+import { AsignarTicket } from '../components/Admin/AsignarTicket'
+import TecnicosDisponibles from '../components/Admin/TecnicosDisponibles'
 
 export const AsignacionTicketPage = () => {
   return (
-    <div className='px-4 py-2'>
-        <TicketsStats/>
-        
+    <div className='py-2'>
+      <TicketsStats />
+      <div className='flex justify-center items-center gap-4 mt-4 flex-col md:flex-row'>
+         <AsignarTicket/>
+         <TecnicosDisponibles/>
+      </div>
+
+
     </div>
   )
 }
